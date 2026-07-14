9d3f8fd8f5f59171367a2d5d989f2a7cee222201
trinhtruong215 <truongtrinh049@gmail.com>
2026-07-14 13:46:38 +0700

    Test: Implement integration tests for Controller-Model interaction

--- tests/integration/controller.test.js
+++ tests/integration/controller.test.js
@@ -21,15 +21,24 @@ describe('Controller-Service Integration Tests', () => {
 
     test('handleAddTodo should call service.addTodo and update the model', () => {
         // TODO: Call the controller's handleAddTodo method with some test text.
+        controller.handleAddTodo("Set alarm clock");
         // Then, get the list of todos directly from the service.
+        expect(service.todos.length).toBe(1);
         // Assert that the service's todos array has a length of 1.
+        expect(service.todos[0].text).toBe("Set alarm clock");
         // Assert that the text of the first todo in the service matches the input.
+        expect(service.todos[0].completed).toBe(false);
+
     });
 
     test('handleRemoveTodo should call service.removeTodo and update the model', () => {
         // TODO: First, directly add a todo to the service.
+         service.addTodo("Buy milk");
         // Get the ID of the new todo.
+        const id = service.todos[0].id;
         // Call the controller's handleRemoveTodo method with that ID.
+        controller.handleRemoveTodo(id);
         // Assert that the service's todos array is now empty.
+        expect(service.todos.length).toBe(0);
     });
 });
